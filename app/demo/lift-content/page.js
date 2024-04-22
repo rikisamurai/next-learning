daddf575f563c59b85fb00cca3c7ac2ffaed209d
riki <shanyulong.riki@bytedance.com>
2024-04-22 16:08:15 +0800

    add lift-content

---
+++ app/demo/lift-content/page.js
@@ -0,0 +1,25 @@
+/**
+ * @see https://overreacted.io/before-you-memo/
+ */
+'use client';
+
+import { useState } from 'react';
+
+export default function App() {
+  let [color, setColor] = useState('red');
+  return (
+    <div>
+      <input value={color} onChange={(e) => setColor(e.target.value)} />
+      <p style={{ color }}>Hello, world!</p>
+      <ExpensiveTree />
+    </div>
+  );
+}
+
+function ExpensiveTree() {
+  let now = performance.now();
+  while (performance.now() - now < 100) {
+    // Artificial delay -- do nothing for 100ms
+  }
+  return <p>I am a very slow component tree.</p>;
+}
